720e36bdf6c2bc0d74dd0c9016ca5cc99011ea64
Jan Weber <jpweber84@gmail.com>
2016-02-17 19:04:29 +0100

    download of complete data; reworked visualization (now I like it)

--- weather_charts.R
+++ weather_charts.R
@@ -3,25 +3,51 @@ checkDataAvailabilityForDateRange("TXL", "1997-01-01", "2015-12-19",
                                   station_type = "airportCode")
 #### DOWNLOAD WEATHER DATA ####
 require(weatherData)
-
+weatherTXL2016 <- getWeatherForYear("TXL", "2016")
+weatherTXL2015 <- getWeatherForYear("TXL", "2015")
 weatherTXL2014 <- getWeatherForYear("TXL", "2014")
 weatherTXL2013 <- getWeatherForYear("TXL", "2013")
 weatherTXL2012 <- getWeatherForYear("TXL", "2012")
 weatherTXL2011 <- getWeatherForYear("TXL", "2011")
 weatherTXL2010 <- getWeatherForYear("TXL", "2010")
 TXL <- getWeatherForDate("TXL", 
-                         start_date = "2013-01-01",
+                         start_date = "1997-01-01",
                          end_date = "2015-12-19",
                          opt_custom_columns = T,
-                         custom_columns = c(1:23))
-#
-#nam <- paste("A", 5, sep = "")
-#assign(nam, TXL)
+                         custom_columns = c(1:23)) %>% 
+        select(-2)
 
-#1997:2013
+### COMPLETE DATA FOR LOCATION TXL FROM 1997 ###
 
+for(i in 1:20){
+if(last_date != today()){
+last_date <- as.Date(max(TXL$Date)) #check latest date available
+
+new_weather_data <- getWeatherForDate("TXL", #get all new data up to today
+                                      start_date = last_date+1,
+                                      end_date = today(),
+                                      opt_custom_columns = T,
+                                      custom_columns = c(1:23))
+new_weather_data<- new_weather_data %>% select(-2)
+new_weatherTXL <- rbind(TXL, new_weather_data) # combine
+TXL <- new_weatherTXL
+last_date <- as.Date(max(TXL$Date))
+}}
+min(TXL$Date)
+max(TXL$Date)
+object.size(TXL)
+
+
+
+names(new_weather_data)
+names(TXL)
+identical(names(new_weather_data), names(TXL))
+comp <- cbind(names(new_weather_data), names(TXL))
+View(comp)
 ### MAKE ONE DATA FRAME ###
-weatherTXL <- rbind(weatherTXL2014,
+weatherTXL <- rbind(weatherTXL2016,
+                    weatherTXL2015,
+                    weatherTXL2014,
                     weatherTXL2013,
                     weatherTXL2012,
                     weatherTXL2011,
@@ -31,12 +57,27 @@ weatherTXL <- rbind(weatherTXL2014,
 save(weatherTXL, file = "weatherTXLfile.RData")
 load("weatherTXLfile.RData")
 
+### UPDATE DATA FRAME
+
+last_date <- as.Date(max(weatherTXL$Date)) #check latest date available
+
+##!! there is a limit on how many days can be retrieved, so this needs to be repeated
+new_weather_data <- getWeatherForDate("TXL", #get all new data up to today
+                                      start_date = last_date+1,
+                                      end_date = today())
+new_weatherTXL <- rbind(weatherTXL, new_weather_data) # combine
+weatherTXL <- new_weatherTXL
+
+
+save(weatherTXL, file = "weatherTXLfile.RData") #save the new data object
+
 ### CLEAN UP WEATHER DATA ###
 require(lubridate)
+names(weatherTXL) <- c("Date", "C_max", "C_mean", "C_min")
 weatherTXL$DayOfYear    <- yday(weatherTXL$Date)
 weatherTXL$Year         <- year(weatherTXL$Date)
 weatherTXL$Month        <- month(weatherTXL$Date, label = T)
-
+weatherTXL$DayOfMonth <- mday(weatherTXL$Date)
 weatherTXL$Date <- as.POSIXct(weatherTXL$Date) # conversion needed, dplyr doesn't like
                                                 # POSIXlt, which is the previous format
 
@@ -58,7 +99,7 @@ p <- p+ geom_bar(stat = "identity")+
 
 print(p)
 
-### 
+### NO IDEA WHAT THIS WAS FOR!
 require(dplyr)
 require(tidyr)
 difference <- weatherTXL %>%
@@ -90,20 +131,16 @@ weatherTXL2014$Month        <- month(weatherTXL2014$Date,
                                  abbr = F)
 weatherTXL2014$DayOfMonth <- mday(weatherTXL2014$Date)
 pdata<- weatherTXL2014
-#pdata$minfive[pdata$Max_TemperatureC <= -5 ] <- -5 
 
-#pdata$Cm5to5[pdata$Max_TemperatureC <= 0 & pdata$Max_TemperatureC >= -5] <- pdata$Max_TemperatureC[pdata$Max_TemperatureC <= 0 & pdata$Max_TemperatureC >= -5]
-#pdata$C0to5[pdata$Max_TemperatureC <=5 & pdata$Max_TemperatureC >=0] <- pdata$Max_TemperatureC[pdata$Max_TemperatureC <=5 & pdata$Max_TemperatureC >=0]
-#pdata$C5to10[pdata$Max_TemperatureC <=10 & pdata$Max_TemperatureC >=5] <- pdata$Max_TemperatureC[pdata$Max_TemperatureC <=10 & pdata$Max_TemperatureC >=5] 
-pdata$Cm5[pdata$Max_TemperatureC<  -5 ] <- -5 
-pdata$C5[pdata$Max_TemperatureC  >   5 ] <-  5 
-pdata$C10[pdata$Max_TemperatureC >  10 ] <- 10
-pdata$C15[pdata$Max_TemperatureC >  15 ] <- 15
-pdata$C20[pdata$Max_TemperatureC >  20 ] <- 20
-pdata$C25[pdata$Max_TemperatureC >  25 ] <- 25
-pdata$C30[pdata$Max_TemperatureC >  30 ] <- 30
+pdata$Cm5[pdata$C_max <  -5 ] <- -5 
+pdata$C5[pdata$C_max  >   5 ] <-  5 
+pdata$C10[pdata$C_max >  10 ] <- 10
+pdata$C15[pdata$C_max >  15 ] <- 15
+pdata$C20[pdata$C_max >  20 ] <- 20
+pdata$C25[pdata$C_max >  25 ] <- 25
+pdata$C30[pdata$C_max >  30 ] <- 30
 # p <- ggplot(pdata, 
-#             aes(x = Date, y = Max_TemperatureC), color = "black") +
+#             aes(x = Date, y = C_max), color = "black") +
 #         geom_bar(stat = "identity")+
 #         geom_bar(stat = "identity", aes(y = twenty), fill="#FF0000")+
 #         geom_bar(stat = "identity", aes(y = ften), fill="#FFFF00")+
@@ -118,7 +155,7 @@ pdata$C30[pdata$Max_TemperatureC >  30 ] <- 30
 # 
 # pdata <- filter(pdata, Month == "February")
 # p <- ggplot(pdata, 
-#             aes(x = Date, y = Max_TemperatureC), colour = "#FF0000", fill = "white") +
+#             aes(x = Date, y = C_max), colour = "#FF0000", fill = "white") +
 #         geom_bar(stat = "identity")+
 #         geom_bar(stat = "identity", aes(y = twenty))+
 #         geom_bar(stat = "identity", aes(y = ften))+
@@ -133,7 +170,7 @@ pdata$C30[pdata$Max_TemperatureC >  30 ] <- 30
 pdata <- filter(pdata, Month == "January")
 thin <- 0.5
 p <- ggplot(pdata, aes(x = DayOfMonth))+
-        geom_bar(stat = "identity", aes(y=Max_TemperatureC), fill = NA, size = 1, color = "black", width = 0.8)+
+        geom_bar(stat = "identity", aes(y=C_max), fill = NA, size = 1, color = "black", width = 0.8)+
         geom_bar(stat = "identity", aes(y=Cm5), fill = NA, size = thin, color = "black", width = 0.8)+
         geom_bar(stat = "identity", aes(y= C5) , fill = NA, size = thin, color = "black", width = 0.8)+
         geom_bar(stat = "identity", aes(y=C10) , fill = NA, size = thin, color = "black", width = 0.8)+
@@ -145,3 +182,25 @@ p <- ggplot(pdata, aes(x = DayOfMonth))+
               panel.grid.major.y = element_blank(),
               panel.grid.minor.y = element_blank())
 p
+
+## TUFTE BY MONTH
+require(ggthemes)
+averages <- weatherTXL %>% 
+        filter(Year >= 2011, Year < 2016) %>% 
+        group_by(Month, DayOfMonth) %>% 
+        summarise(C_5y_average = mean(C_max),
+               C_5y_max = min(C_max),
+               C_5y_min = max(C_max)) %>%
+        ungroup()
+
+weatherTXL %>% 
+        filter(Year == 2016) %>% 
+        ggplot(aes(x = DayOfMonth, y = C_max))+
+        geom_bar(stat = "identity", width = 0.5, fill = "grey40")+
+        #geom_hline(yintercept = seq(-10,25,5), color= "white")+ # does not work as I wanted, because grid is overplotted
+        geom_linerange(data = averages, aes(ymin=C_5y_min, ymax=C_5y_max))+
+        geom_point(data = averages, aes(y = C_5y_average))+
+        #scale_x_continuous(breaks = 1:max(weatherTXL$DayOfMonth))+
+        geom_hline(yintercept = 0)+
+        theme_bw()+
+        facet_wrap(~Month, ncol=4, scales = "free")
